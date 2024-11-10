7f06903c7bce3d61e3d98ef22a953d1e05e237fc
Prathamesh Khade <121573184+Pk-Kolhapurkar@users.noreply.github.com>
2024-11-10 20:22:43 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,83 +1,35 @@
 import streamlit as st
-from streamlit_chat import message
-# from langchain.llms import OpenAI   #This import has been replaced by the below import please
-from langchain_openai import OpenAI
-from langchain.chains import ConversationChain
-from langchain.chains.conversation.memory import (ConversationBufferMemory, 
-                                                  ConversationSummaryMemory, 
-                                                  ConversationBufferWindowMemory
-               
-                                                  )
-
-if 'conversation' not in st.session_state:
-    st.session_state['conversation'] =None
-if 'messages' not in st.session_state:
-    st.session_state['messages'] =[]
-if 'API_Key' not in st.session_state:
-    st.session_state['API_Key'] =''
-
-# Setting page title and header
-st.set_page_config(page_title="Chat GPT Clone", page_icon=":robot_face:")
-st.markdown("<h1 style='text-align: center;'>How can I assist you? </h1>", unsafe_allow_html=True)
+from utils import *
 
 
-st.sidebar.title("😎")
-st.session_state['API_Key']= st.sidebar.text_input("What's your API key?",type="password")
-summarise_button = st.sidebar.button("Summarise the conversation", key="summarise")
-if summarise_button:
-    summarise_placeholder = st.sidebar.write("Nice chatting with you my friend ❤️:\n\n"+st.session_state['conversation'].memory.buffer)
-    #summarise_placeholder.write("Nice chatting with you my friend ❤️:\n\n"+st.session_state['conversation'].memory.buffer)
+# Define the Streamlit app
+def main():
+    st.title("Customer Care Call Summarization")
 
-#import os
-#os.environ["OPENAI_API_KEY"] = "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"
+    # Upload multiple files
+    uploaded_files = st.file_uploader("Upload recorded .mp3 files", type=["mp3"], accept_multiple_files=True)
 
-def getresponse(userInput, api_key):
+    if uploaded_files:
+        st.write("Uploaded Files:")
 
-    if st.session_state['conversation'] is None:
-
-        llm = OpenAI(
-            temperature=0,
-            openai_api_key=api_key,
-            model_name='gpt-3.5-turbo-instruct'  # 'text-davinci-003' model is depreciated now, so we are using the openai's recommended model
-        )
-
-        st.session_state['conversation'] = ConversationChain(
-            llm=llm,
-            verbose=True,
-            memory=ConversationSummaryMemory(llm=llm)
-        )
-
-    response=st.session_state['conversation'].predict(input=userInput)
-    print(st.session_state['conversation'].memory.buffer)
-    
-
-    return response
-
-
-
-response_container = st.container()
-# Here we will have a container for user input text box
-container = st.container()
-
-
-with container:
-    with st.form(key='my_form', clear_on_submit=True):
-        user_input = st.text_area("Your question goes here:", key='input', height=100)
-        submit_button = st.form_submit_button(label='Send')
-
-        if submit_button:
-            st.session_state['messages'].append(user_input)
-            model_response=getresponse(user_input,st.session_state['API_Key'])
-            st.session_state['messages'].append(model_response)
+        # Display uploaded files and buttons in a tabular form
+        for uploaded_file in uploaded_files:
+            file_name = uploaded_file.name
             
 
-            with response_container:
-                for i in range(len(st.session_state['messages'])):
-                        if (i % 2) == 0:
-                            message(st.session_state['messages'][i], is_user=True, key=str(i) + '_user')
-                        else:
-                            message(st.session_state['messages'][i], key=str(i) + '_AI')
-        
-             
+            col1, col2, col3 = st.columns([0.1, 1, 2])
+            with col1:
+                st.write("-")
+            with col2:
+                st.write(file_name)
+            with col3:
+                send_button = st.button(f"Send Email for {file_name}")
 
-        
+                if send_button:
+                    email_summary(file_name)
+                    st.success(f"Send email for: {file_name}")
+
+
+# Run the Streamlit app
+if __name__ == "__main__":
+    main()
